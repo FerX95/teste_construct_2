01318278dfdcf1a0fbfd15300c99b9e3820d5ced
Fernando Silva <fernando.jmelo39@gmail.com>
2023-10-19 01:27:49 -0300

    teste 01

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1697689486,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/spr_player-sheet0.png",
+		"images/spr_plataforma-sheet0.png",
+		"images/spr_spiks.png",
+		"images/part_blood.png",
+		"images/spr_ground.png",
+		"images/bg_level_01.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
